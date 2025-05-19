034ff37efc608bb42f071f2cfbf2b30613e23786
Rizqi Alfaruq <43517239+alfaruqrizqi18@users.noreply.github.com>
2025-05-20 05:57:24 +0700

    feat: added utils.dart

---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,29 @@
+// screen size
+import 'package:flutter/material.dart';
+
+import '../services/navigation_service.dart';
+
+const double generalPaddingHorizontal = 25;
+
+Widget vSpace(double val) {
+  return SizedBox(height: val);
+}
+
+Widget hSpace(double val) {
+  return SizedBox(width: val);
+}
+
+double screenWidth() => MediaQuery.of(globalCurrentContext).size.width;
+double screenHeight() => MediaQuery.of(globalCurrentContext).size.height;
+Size screenSize() => MediaQuery.of(globalCurrentContext).size;
+
+
+String getLanguageFromLangCode(String lang) {
+  if (lang == "en") {
+    return "English";
+  } else if (lang == "id") {
+    return "Indonesia";
+  } else {
+    return "";
+  }
+}
