6d1db5a55849ebf6d6557673a869f8fd101d1868
Rizqi Alfaruq <43517239+alfaruqrizqi18@users.noreply.github.com>
2025-05-20 06:00:27 +0700

    chore: update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,39 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:minichatapp/core/utils/print_log.dart';
-import 'package:minichatapp/firebase_options.dart';
 import 'package:provider/provider.dart';
+
+import 'app.dart';
+import 'core/services/hive_service.dart';
+import 'core/utils/print_log.dart';
+import 'features/auth/providers/app_auth_provider.dart';
+import 'firebase_options.dart';
+import 'injection.dart';
 
 void main() {
   runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
 
-      // register AppRuter in GetIt
-      // GetIt.instance.registerSingleton<AppRouter>(AppRouter());
-
       // init firebase
       await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
       );
 
-      // runApp
-      runApp(MultiProvider(providers: [], child: Container()));
+      AppInjections().inject();
+
+      await CosmoHive().init();
+
+      runApp(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => AppInjections().injection.get<AppAuthProvider>(),
+            ),
+          ],
+          child: CosmoApp(),
+        ),
+      );
     },
     (error, stackTrace) async {
       printLog('runZonedGuarded error: $error, $stackTrace');
