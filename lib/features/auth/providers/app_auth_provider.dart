8d0de454c0db9fdf003b536f766441ae0511b000
Rizqi Alfaruq <43517239+alfaruqrizqi18@users.noreply.github.com>
2025-05-20 05:59:01 +0700

    feat: added AppAuthProvider

---
+++ lib/features/auth/providers/app_auth_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+
+import '../../../core/models/response_state.dart';
+import '../../../core/repositories/app_repository.dart';
+import '../../../core/services/google_signin_service.dart';
+
+class AppAuthProvider extends ChangeNotifier {
+  Future<ResponseState> signIn() async {
+    final request = await GoogleSignInService().signInGoogle();
+    if (request.success) {
+      // set app state logged in
+      AppRepository.setLoggedIn(loggedIn: true);
+
+      // set current user
+      AppRepository.setCurrentUser(
+        user: GoogleSignInService().getLoggedInAccount(),
+      );
+      return ResponseState(success: true);
+    } else {
+      return ResponseState(success: false);
+    }
+  }
+
+  Future<void> signOut() async {
+    await GoogleSignInService().logout();
+    AppRepository.logout();
+  }
+}
