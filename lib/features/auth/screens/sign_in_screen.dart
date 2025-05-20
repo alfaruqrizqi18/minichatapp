4cfdd7c07d2018f5afd91a46f235a680aee035a7
Rizqi Alfaruq <43517239+alfaruqrizqi18@users.noreply.github.com>
2025-05-21 03:17:57 +0700

    chore: refactoring sign in screen

--- lib/features/auth/screens/sign_in_screen.dart
+++ lib/features/auth/screens/sign_in_screen.dart
@@ -9,6 +9,7 @@ import '../../../core/widgets/app_bar/app_bar_v1.dart';
 import '../../../core/widgets/buttons/cosmo_filled_button.dart';
 import '../../../core/widgets/images/cosmo_asset_image.dart';
 import '../../../generated/assets.gen.dart';
+import '../../../routes/app_routing.gr.dart';
 import '../providers/app_auth_provider.dart';
 
 @RoutePage()
@@ -26,48 +27,63 @@ class _SignInScreenState extends State<SignInScreen> {
       appBar: CosmoAppBar(
         title: Hero(
           tag: 'logo',
-          child: FlutterLogo(size: 20, textColor: context.primaryColor),
+          child: FlutterLogo(size: 30, textColor: context.primaryColor),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text.rich(
-                TextSpan(
-                  text: "Mini Chat",
-                  style: GoogleFonts.dmSerifDisplay().copyWith(
-                    color: context.onSurfaceColor,
-                    fontSize: 30,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: "App",
-                      style: GoogleFonts.dmSerifDisplay().copyWith(
-                        color: context.primaryColor,
-                        fontSize: 30,
-                      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: generalPaddingHorizontal),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Mini Chat",
+                    style: GoogleFonts.dmSerifDisplay().copyWith(
+                      color: context.onSurfaceColor,
+                      fontSize: 30,
                     ),
-                  ],
+                    children: [
+                      TextSpan(
+                        text: "App",
+                        style: GoogleFonts.dmSerifDisplay().copyWith(
+                          color: context.primaryColor,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
+              ],
+            ),
+            vSpace(screenHeight() * .02),
+            Text(
+              "Mini Chat App is a real-time chat application designed as a proof of concept to demonstrate technical capabilities, particularly in handling real-time data, user authentication, and building an intuitive user interface",
+              style: context.bodyMedium?.copyWith(color: context.onSurfaceColor),
+              textAlign: TextAlign.center,
+            ),
+            vSpace(screenHeight() * .06),
+            SizedBox(
+              width: screenWidth() * 0.7,
+              child: CosmoFilledButton(
+                onPressed: () async {
+                  final request = await context.read<AppAuthProvider>().signIn();
+                  if (request.success) {
+                    if (!context.mounted) return;
+                    context.router.replaceAll([LandingRoute()]);
+                  }
+                },
+                buttonColor: context.surfaceColor,
+                title: "Sign in with Google",
+                textColor: context.onSurfaceColor,
+                icon: CosmoAssetImage(path: Assets.icons.google.path, width: 25),
               ),
-            ],
-          ),
-          vSpace(screenHeight() * .06),
-          CosmoFilledButton(
-            onPressed: () async {
-              final authP = context.read<AppAuthProvider>();
-              authP.signIn();
-            },
-            buttonColor: context.surfaceColor,
-            title: "Sign in with Google",
-            textColor: context.onSurfaceColor,
-            icon: CosmoAssetImage(path: Assets.icons.google.path, width: 25),
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
